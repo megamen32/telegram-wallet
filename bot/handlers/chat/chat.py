38faafb8bb4d8413b11dcda5563cdae67b32c207
Nickname <megamen932@gmail.com>
2022-11-26 01:47:09 +0300

    v0.063  chat can be turned off

--- bot/handlers/chat/chat.py
+++ bot/handlers/chat/chat.py
@@ -22,3 +22,4 @@ async def say_all(message: Message, user: User, state: FSMContext):
     users = User.select().where(User.person != None)
     for us in users:
         asyncio.create_task(bot.send_message(us.id, f'{user.person}: {message.text}'))
+    await state.reset_state()
