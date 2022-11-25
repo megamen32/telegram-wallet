5cf16b7e7d1a81ae2928a7416cf70e89da43190c
Nickname <megamen932@gmail.com>
2022-11-26 01:19:50 +0300

    v0.062 add chat

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,4 +1,6 @@
 from .errors import dp
 from .users import dp
+from .wallet import dp
+from .chat import dp
 
 __all__ = ['dp']
---
+++ bot/handlers/chat/chat.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from loader import dp, bot
+from models import User
+
+desk_st = State('say_all')
+
+
+@dp.message_handler(i18n_text='Сказать Всем')
+async def init_say_all(message: Message):
+    await dp.current_state().set_state(desk_st)
+    await message.answer('Напишите что отправить всем', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(commands='say', state='*')
+@dp.message_handler(state=desk_st)
+async def say_all(message: Message, user: User, state: FSMContext):
+    users = User.select().where(User.person != None)
+    for us in users:
+        asyncio.create_task(bot.send_message(us.id, f'{user.person}: {message.text}'))
