9e867f2db1cb9285a8b36f04ec0f6b72070e578c
Nickname <megamen932@gmail.com>
2022-12-06 20:19:55 +0300

    v0.0754 walet markdown

--- bot/handlers/wallet/wallet.py
+++ bot/handlers/wallet/wallet.py
@@ -28,8 +28,8 @@ async def wallet_handler(message:types.Message,user:User):
                  .group_by(Person)
                  .order_by(fn.COUNT(Expanse.id).desc()))
         for person in query:
-            text=(_('%s -- %s трат, на сумму %s') % (person.name, person.transaction_count,person.transaction_amount,))
-            await message.answer(text)
+            text=(_('*%s* – *%s* трат на сумму: *%s*') % (person.name, person.transaction_count,person.transaction_amount,))
+            await message.answer(text,parse_mode='Markdown')
         wallet=user.wallet
 
         incomes=Income.select().where(Income.wallet == wallet).order_by(Income.created_at)
@@ -40,10 +40,10 @@ async def wallet_handler(message:types.Message,user:User):
             total_sum+=tr.amount
             exps_amount = tr.get_expanses_amount()
             total_expense+= exps_amount
-            text_ = f'Поступление {tr.amount} {tr.description} {tr.created_at.strftime("%d/%m/%Y, %H:%M")} Осталось:{tr.amount-exps_amount}{text}'
-            await message.reply(text_)
+            text_ = f'*{tr.description}*\n*Остаток: *{tr.amount-exps_amount}*\n\nПоступление *{tr.amount}* {tr.created_at.strftime("%d/%m/%Y, %H:%M")}\n\n{text}*'
+            await message.reply(text_,parse_mode='Markdown')
 
-        await message.answer(f'Доход {total_sum} Расход {total_expense} Итоговый баланс {total_sum-total_expense}',reply_markup=get_default_markup(user))
+        await message.answer(f'*Текущий баланс: {total_sum-total_expense}*\n\nДоход: *{total_sum}*\nРасход: *{total_expense}*',parse_mode='Markdown',reply_markup=get_default_markup(user))
     except:
         err=traceback.format_exc()
         await message.answer(err)
