018390fd05837049ec4b01011264d86554b9bd86
Nickname <megamen932@gmail.com>
2022-12-09 20:04:48 +0300

    v0.08569 reamke was_used in bids

--- bot/handlers/wallet/expense.py
+++ bot/handlers/wallet/expense.py
@@ -26,7 +26,7 @@ async def new_expanse_handler(message: Message, user: User,state:FSMContext):
         texts=''
         for i,bid in enumerate(bids):
             if bid.was_used:continue
-            totals=bid.get_expenses_amount()
+            totals=bid.sum
             texts += f'{i}) {bid.author.name} {bid.amount}-{totals}={bid.amount-totals} id:{bid.id} {bid.description}\n'
             kb=InlineKeyboardButton(f"{i} {bid.amount} id:{bid.id}", callback_data=bid_cb.new(bid=bid.id))
             markup.add(kb)
