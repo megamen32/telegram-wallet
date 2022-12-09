129c7b61d798c5d69e047772997ee74bae2f47a8
Nickname <megamen932@gmail.com>
2022-12-09 03:16:40 +0300

    v0.0855

--- bot/handlers/wallet/wallet.py
+++ bot/handlers/wallet/wallet.py
@@ -83,7 +83,7 @@ async def add_wallet_user(message: types.Message, user: User):
     try:
         wallet=user.wallet
         users_all =list( (User
-     .select()
+     .select().join(Person)
      .where(~fn.EXISTS(
           WalletPermission.select().where(
               (WalletPermission.wallet == wallet) )))))
